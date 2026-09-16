3cc3204ca77302d12e388940d4a38074019ad14e
bhavna.proses <bhavna.proses@gmail.com>
2026-09-16 14:04:55 +0530

    change logo and spalsh screen , logo change

---
+++ src/assets/images/index.ts
@@ -0,0 +1,8 @@
+export const IMAGES = {
+  logo: require('./logo/gamelivo_logo_v2.png'),
+  logoV2: require('./logo/gamelivo_logo_v2.png'),
+  logoFull: require('./logo/gamelivo_logo_full.png'),
+  logoIcon: require('./icons/GameLivoIcon.png'),
+  logoWithoutBg: require('./logo/gamelivo_logo_v2.png'),
+  logoV1WithoutBg: require('./icons/GameLivowithoutbg.png'),
+};
